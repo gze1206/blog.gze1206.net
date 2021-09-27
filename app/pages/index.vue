33b364366e3ba9b1ba4d7f456dbcfee265fad076
gze1206 <gze1206@naver.com>
2021-09-27 18:45:05 +0900

    Cleanup layouts

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,77 +1,34 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div class="noselect" style="min-height: 3000px!important;">
+    <v-parallax id="main-hero" dark src="/img/hero1.jpg" height="400">
+      <v-row align="center" justify="center">
+        <v-col cols="12" class="text-center">
+          <h1 class="display-1 font-weight-bold mb-4">
+            {{ title }}
+          </h1>
+        </v-col>
+      </v-row>
+    </v-parallax>
+  </div>
 </template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      title: 'blog.gze1206.net'
+    }
+  }
+}
+</script>
+
+<style scoped>
+#main-hero {
+  left: 0;
+  top: 0;
+  position: absolute;
+  width: 100%;
+  background-color: #00000077;
+}
+</style>
