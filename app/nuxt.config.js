4c7df9ece01ad7dbecf812186cbdd221c733dd56
gze1206 <gze1206@naver.com>
2021-10-01 03:54:50 +0900

    Change to static website

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -30,6 +30,8 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
+  target: 'static',
+
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
