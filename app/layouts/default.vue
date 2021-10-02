1dd3490bc940633ed878fe15d051ff2ab0f3fb0f
gze1206 <gze1206@naver.com>
2021-10-02 10:48:48 +0900

    Add appbar

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,19 @@
 <template>
-  <v-app dark>
+  <v-app dark class="noselect">
+    <v-app-bar id="main-appbar" fixed app>
+      <v-btn plain outlined to="/" v-text="title" />
+      <v-spacer />
+      <v-btn
+        v-for="menu in menus"
+        :key="menu.to"
+        plain
+        class="route-btn"
+        active-class="active-route"
+        :to="menu.to"
+        :exact="menu.exact"
+        v-text="menu.text"
+      />
+    </v-app-bar>
     <v-main>
       <v-container>
         <Nuxt />
@@ -20,12 +34,10 @@
 export default {
   data () {
     return {
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        }
+      title: 'blog.gze1206.net',
+      menus: [
+        { text: 'categories', to: '/categories', exact: false },
+        { text: 'blog', to: '/blog', exact: true }
       ]
     }
   }
