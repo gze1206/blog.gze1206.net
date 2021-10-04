3221594b476a19595c4ad887e5dc939f7329512b
gze1206 <gze1206@naver.com>
2021-10-05 00:29:43 +0900

    Title template setup

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -3,6 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
+    title: '',
     titleTemplate: (titleChunk) => {
       const siteTitle = 'blog.gze1206.net'
       return titleChunk ? `${titleChunk} - ${siteTitle}` : siteTitle
