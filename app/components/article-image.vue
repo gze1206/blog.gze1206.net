49527cec670adc73d3f868966f9971f759b8175e
gze1206 <gze1206@naver.com>
2021-10-05 00:06:34 +0900

    Add article image component

---
+++ app/components/article-image.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="full-width article-image mt-4">
+    <if-wrap>
+      <a :if="to" :href="to" target="_blank">
+        <v-img :src="src" lazy-src="/img/hero1.jpg">
+          <template #placeholder>
+            <v-row
+              class="fill-height ma-0"
+              align="center"
+              justify="center"
+            >
+              <v-progress-circular
+                indeterminate
+                color="grey lighten-5"
+              />
+            </v-row>
+          </template>
+        </v-img>
+      </a>
+    </if-wrap>
+    <p v-if="text || ref" class="text-caption text-center">
+      {{ text }}
+      <a v-if="ref" :href="ref.link" target="_blank">
+        출처 : {{ ref.label }}
+      </a>
+    </p>
+  </div>
+</template>
+
+<script>
+import ifWrap from './if-wrap.vue'
+export default {
+  components: { ifWrap },
+  props: {
+    to: {
+      type: String,
+      default: () => ''
+    },
+    src: {
+      type: String,
+      default: () => ''
+    },
+    text: {
+      type: String,
+      default: () => ''
+    },
+    refLabel: {
+      type: String,
+      default: () => ''
+    },
+    refLink: {
+      type: String,
+      default: () => ''
+    }
+  },
+  computed: {
+    ref () {
+      if (!(this.refLabel && this.refLink)) { return null }
+      return {
+        label: this.refLabel,
+        link: this.refLink
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ app/components/if-wrap.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  functional: true,
+  props: {
+    if: {
+      type: [Boolean, Function],
+      default: () => false
+    }
+  },
+  render (h, ctx) {
+    const children = ctx.children.filter(v => v.tag)
+    if (children.length !== 1) {
+      // eslint-disable-next-line no-console
+      console.warn('this component accepts only one root node in its slot')
+    }
+
+    return ctx.props.if ? children[0] : children[0].children
+  }
+}
+</script>
+
+<style>
+
+</style>
