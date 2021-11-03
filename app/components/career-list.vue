08b6c026ccef49a4be03fec3585f87fdbf128707
gze1206 <gze1206@naver.com>
2021-11-04 02:12:15 +0900

    Add career detail view

--- app/components/career-list.vue
+++ app/components/career-list.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <v-card v-for="item in careers" :key="item.order" outlined class="my-3" max-width="500px">
+    <v-card
+      v-for="item in careers"
+      :key="item.order"
+      outlined
+      class="my-3"
+      max-width="500px"
+      @click="open(item)"
+    >
       <lazy-image v-if="item.cover" :src="item.cover" />
       <v-card-title>{{ item.title }}</v-card-title>
       <v-card-subtitle>{{ formatDuration(item) }}</v-card-subtitle>
@@ -10,7 +17,29 @@
           {{ item.project }}
         </div>
       </v-card-text>
+      <v-card-actions class="text-right">
+        <v-card-text>
+          More...
+        </v-card-text>
+      </v-card-actions>
     </v-card>
+
+    <v-dialog v-model="dialog" scrollable max-width="80%" @click:outside="close">
+      <v-card v-if="selected">
+        <lazy-image v-if="selected.cover" :src="selected.cover" />
+        <v-card-title>{{ selected.title }}</v-card-title>
+        <v-card-subtitle class="pt-1">
+          Working period : {{ formatDuration(selected) }}
+        </v-card-subtitle>
+        <v-card-text style="color: inherit">
+          Department : {{ selected.department }}
+          <div v-if="selected.project">
+            Project : {{ selected.project }}
+          </div>
+        </v-card-text>
+        <nuxt-content class="ma-5 mt-0" :document="selected" />
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -32,12 +61,30 @@ export default {
       default: () => []
     }
   },
+  data () {
+    return {
+      dialog: false,
+      selected: null
+    }
+  },
   methods: {
     formatDuration (item) {
       const from = new Date(item.from)
       const to = item.to ? new Date(item.to) : null
 
       return `${dateFormat(from)} ~ ${to ? dateFormat(to) : '재직 중'}`
+    },
+    open (item) {
+      if (item == null) {
+        return
+      }
+
+      this.dialog = true
+      this.selected = item
+    },
+    close () {
+      this.dialog = false
+      this.selected = null
     }
   }
 }
